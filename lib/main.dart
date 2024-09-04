d14c1bd86f6bab4f24ff9430687cbf69ea9b3782
BerfinGoksen <berfingoksen24@gmail.com>
2024-09-04 11:46:47 +0300

    first_commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
 import 'package:books_app/book/Splash.dart';
-import 'package:books_app/book/home.dart';
 import 'package:flutter/material.dart';
-
-import 'book/details.dart';
 
 void main() => runApp(const MyApp());
 
